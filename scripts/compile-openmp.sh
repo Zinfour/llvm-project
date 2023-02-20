6ea1b57424aae0be7120bbe69e3e5755f84cfd97
zinfour <zinfour17@gmail.com>
2023-02-20 13:08:19 +0100

    remove hardcoded values from compile-openmp.sh

--- scripts/compile-openmp.sh
+++ scripts/compile-openmp.sh
@@ -4,9 +4,9 @@
 
 cd openmp/build
 
-cmake -DCMAKE_BUILD_TYPE=Debug -DLIBOMP_MOLDABILITY=ON -DOPENMP_LLVM_TOOLS_DIR=/home/zinfour/Hackstuff/llvm-project/build/bin ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DLIBOMP_MOLDABILITY=ON -DOPENMP_LLVM_TOOLS_DIR=$(pwd)/build/bin ..
 
-make -j 32
+make -j $(nproc)
 
 cd ../..
 
